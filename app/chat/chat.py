07a0f980945c5968d8f3d78a9236e54612d34253
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-04-03 14:15:26 +0200

    build retriever

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,16 +1,17 @@
 from app.chat.models import ChatArgs
+from app.chat.vector_stores.pinecone import build_retriever
+
+"""
+:param chat_args: ChatArgs object containing
+    conversation_id, pdf_id, metadata, and streaming flag.
+
+:return: A chain
+
+Example Usage:
+
+    chain = build_chat(chat_args)
+"""
 
 
 def build_chat(chat_args: ChatArgs):
-    """
-    :param chat_args: ChatArgs object containing
-        conversation_id, pdf_id, metadata, and streaming flag.
-
-    :return: A chain
-
-    Example Usage:
-
-        chain = build_chat(chat_args)
-    """
-
-    pass
+    retriever = build_retriever(chat_args)
--- app/chat/vector_stores/pinecone.py
+++ app/chat/vector_stores/pinecone.py
@@ -10,3 +10,12 @@ pinecone_client = PineconeClient(api_key=os.getenv("PINECONE_API_KEY"))
 vector_store = LangchainPinecone.from_existing_index(
     os.getenv("PINECONE_INDEX_NAME"), embeddings
 )
+
+
+def build_retriever(chat_args):
+    search_kwargs = {"filter": {
+        "pdf_id": chat_args.pdf_id
+    }}
+    return vector_store.as_retriever(
+        search_kwargs=search_kwargs
+    )
